cd8f35ae970c8f19047a7b0e6989b2399b8ce71a
Vishal Wadgaonkar <wadgaonkarvishal54@gmail.com>
2026-09-08 23:47:41 +0530

    feat(frontend): scaffold react-ts app and finalize docs

---
+++ frontend/vite.config.ts
@@ -0,0 +1,43 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path/win32'
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(), 
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Rekindle",
+        short_name: "Rekindle",
+        theme_color: "#your-color",
+
+        icons: [
+          {
+            src: "/icons/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    environment: "jsdom",
+  },
+})
